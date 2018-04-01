039ad445a354ba1d4c684376c208947da294c246
ContributorKshitij <36894914+ContributorKshitij@users.noreply.github.com>
2018-04-01 21:30:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,9 @@ var Template =`<html>
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/test-db',function (req,res){
+    
+});
 
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
