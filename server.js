d0518879de7b24b50f09e1d1db95595843dbe57d
ContributorKshitij <36894914+ContributorKshitij@users.noreply.github.com>
2018-04-01 23:49:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,147 +2,170 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var config = {
-    user:'kshitijmishra0',
-    database:'kshitijmishra0',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
-};
+var config ={
+             user:'kshitijmishra0',
+             database:'kshitijmishra0',
+             host:'db.imad.hasura-app.io',
+             port:'5432',
+             password:process.env.DB_PASSWORD
+            };
 var pool = new Pool(config);
 
 var app = express();
 app.use(morgan('combined'));
 var articles = {
-     'article-one': { title: 'article-one',
-        heading: 'article-one',
-        date: '9 march 2018',
-        content: ` <p>
-                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
-            </p>
-        </div>
-        <div>
-            <p>
-                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
-            </p>`
-    },
-     'article-two': {title: 'article-two',
-        heading: 'article-two',
-        date: '9 march 2018',
-        content: ` 
-               <p>
-                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
-            </p>
-        </div>
-        <div>
-            <p>
-                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
-            </p>`
+                 'article-one': 
+                               { title: 'article-one',
+                                 heading: 'article-one',
+                                 date: '9 march 2018',
+                                 content: ` <p>
+                                                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
+                                            </p>  
         
- },
-     'article-three': {  title: 'article-three',
-        heading: 'article-three',
-        date: '9 march 2018',
-        content: ` 
-               <p>
-                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
-            </p>
-        </div>
-        <div>
-            <p>
-                this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
-            </p>`}
-    };
+                              
+                                            <p>
+                                               this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
+                                           </p>`
+                                },
+                 'article-two': 
+                               { title: 'article-two',
+                                 heading: 'article-two',
+                                 date: '9 march 2018',
+                                 content: `<p>
+                                              this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
+                                          </p>
+         
+                                          
+                                          <p>
+                                             this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
+                                          </p>`
+        
+                                },
+                 'article-three': {  title: 'article-three',
+                                     heading: 'article-three',
+                                     date: '9 march 2018',
+                                     content: `<p>
+                                                 this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
+                                               </p>
+        
+        
+                                               <p>
+                                                  this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.this is a paragraph.
+                                              </p>`
+                     
+                                  }
+                };
 
-function createTemplate(data){
-var title = data.title;
-var heading = data.heading;
-var content = data.heading;
-var date = data.date;
-var Template =`<html>
-<head>
-     <link href="/ui/style.css" rel="stylesheet" />
-    <title>${title}</title>
-    <meta name="viewport"  content="width-device-width,initia-scale-1"/>
-</head>    
-<body>
-    <div class="container">
-    <div>
-    <a href="/">HOme</a>
-    </div>
-    <hr>
-    <div>
-    <h2>${heading} </h2>
-    </div>
-    <div>${date}</div>
-    <div>
-       ${content}
-    </div>
-    </div>
-</body>
-</html>
-
-`;
-    return Template;
-}
+function createTemplate(data)
+    {
+        var title = data.title;
+        var heading = data.heading;
+        var content = data.heading;
+        var date = data.date;
+        var Template =`<html>
+                                <head>
+                                     <link href="/ui/style.css" rel="stylesheet" />
+                                    <title>${title}</title>
+                                    <meta name="viewport"  content="width-device-width,initia-scale-1"/>
+                                </head>    
+                            <body>
+                                <div class="container">
+                                <div>
+                                <a href="/">HOme</a>
+                                </div>
+                                <hr>
+                                <div>
+                                <h2>${heading} </h2>
+                                </div>
+                                <div>${date}</div>
+                                <div>
+                                   ${content}
+                                </div>
+                                </div>
+                            </body>
+                        </html>`;
+        return Template;
+        }
 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/test-db',function (req,res){
-    pool.query('SELECT * FROM test',function(err,result){
-        if(err){
-            res.status(500).send(err.toString());
-        } else{
-            res.send(JSON.stringify(result.rows));
-        }   
-    });
+app.get('/test-db',function (req,res)
+                                {
+                                    pool.query('SELECT * FROM test',function(err,result)
+                                                                        {
+                                                                            if(err)
+                                                                            {
+                                                                                res.status(500).send(err.toString());
+                                                                            } 
+                                                                            else
+                                                                            {
+                                                                                res.send(JSON.stringify(result.rows));
+                                                                            }   
+                                                                         });
     
-});
+                                 });
 
 app.get('/article/:articleName',function(req,res)
-{
+                                    {
     
-    pool.query("SELECT * FROM article WHERE title='"+req.paraem.articleName+"'" ,function(err,result)
-    {
-       if(err){
-           res.status(500).send(err.toString());
-       } else{
-           if(result.row.length===0){
-               res.status(404).send('Article not found');
-           }else{
-               var articleData=result.rows[0];
-               res.send(createTemplate(articleData));
-           }
-           
-       }
-    });
+                                        pool.query("SELECT * FROM article WHERE title='"+req.paraem.articleName+"'" ,  function(err,result)
+                                                {
+                                                   if(err)
+                                                   {
+                                                       res.status(500).send(err.toString());
+                                                   } 
+                                                   else
+                                                   {
+                                                       if(result.row.length===0)
+                                                       {
+                                                           res.status(404).send('Article not found');
+                                                       }else
+                                                       {
+                                                           var articleData=result.rows[0];
+                                                           res.send(createTemplate(articleData));
+                                                       }
+                                                       
+                                                   }
+                                                }
+                                            );
+                                       
    
-   
-});
+                                    }
+        );
 
 
-app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/article-two',function(req,res)
+                        {
+                            res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
    
-});
-app.get('/article-three',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+                        }
+        );
+app.get('/article-three',function(req,res)
+                            {
+                                res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+                            }
+        );
+app.get('/ui/style.css', function (req, res) 
+                            {
+                                res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+                            }
+        );
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+app.get('/ui/madi.png', function (req, res) 
+                            {
+                                res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+                            }
+        );
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
 var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+app.listen(port, function () 
+                    {
+                        console.log(`IMAD course app listening on port ${port}!`);
+                    }
+           );
